bdba0f22c34227b47651e21779585590580d755e
khanna01 <govmfkdtm@gmail.com>
2025-05-10 19:56:28 +0900

    [기본타입] 원시타입과 리터럴 타입

---
+++ section2/src/ch1.ts
@@ -0,0 +1,21 @@
+// number
+let num1: number = 123;
+let num2: number = -123;
+let num3: number = 0.123;
+let num4: number = -0.123;
+let num5: number = Infinity;
+let num6: number = -Infinity;
+let num7: number = NaN;
+
+// 엄격한 null 검사 false => 숫자나 null 허용
+//let num: number = null;
+
+// 리터럴
+// 숫자 10을 타입으로
+let num: 10 = 10;
+
+// 문자값을 타입으로
+let str: 'hello' = 'hello';
+
+// 불리언값을 타입으로
+let bool: true = true;
